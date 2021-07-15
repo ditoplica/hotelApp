95e9ef1b7e35bf2d7fd3c5b0088c97d093cb8db2
EToplica <endrit.toplica@outlook.com>
2021-07-15 17:11:54 +0200

    FirstCommit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+const Stack = createStackNavigator();
+import HomeScreen from './views/screens/HomeScreen';
+import DetailsScreen from './views/screens/DetailsScreen';
+import COLORS from './assets/colors/COLORS';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <StatusBar backgroundColor={COLORS.white} barStyle="dark-content" />
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
